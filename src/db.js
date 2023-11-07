7af4883c5ea5c99c761e8e62721a13f8c8164b0c
pakomercado0517 <sissyyayle0517@hotmail.com>
2023-11-07 00:13:47 -0600

    logging  url

--- src/db.js
+++ src/db.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const { DATABASE_URL } = process.env;
 
+console.log("DATABASE_URL", DATABASE_URL);
+
 // let sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
