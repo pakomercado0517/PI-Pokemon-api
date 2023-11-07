b8ab1593bbaee3b0bcac33530f9ca09453938aef
pakomercado0517 <sissyyayle0517@hotmail.com>
2023-11-06 23:06:52 -0600

    enviroment variables

--- src/db.js
+++ src/db.js
@@ -8,14 +8,14 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
-let sequelize = new Sequelize(`${DATABASE_URL}`, {
+let sequelize = new Sequelize(`${DB_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  // dialectOptions: {
-  //   ssl: {
-  //     rejectUnauthorized: false,
-  //   },
-  // },
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 });
 const basename = path.basename(__filename);
 
