dbb14b21e9701ac3667a59536d76d9ec05b1aabf
pakomercado0517 <sissyyayle0517@hotmail.com>
2023-11-08 18:27:51 -0600

    change headers to cors framework

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
+const cors = require("cors");
 
 require("./db.js");
 
@@ -14,17 +15,25 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "pako-pokedex.surge.sh"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Origin", "localhost:5174"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+server.use(
+  cors({
+    origin: ["https://pako-pokedex.surge.sh", "http://localhost:5174"],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+// server.use((req, res, next) => {
+//   // res.header("Access-Control-Allow-Origin", "pako-pokedex.surge.sh"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 server.use("/", routes);
 
